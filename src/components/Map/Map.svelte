7ed196b73b4a53ce6ae676cffeddbe81cc00fb1f
muskanegiz <muskan@egizsolution.com>
2023-03-10 16:29:55 +0530

    map

--- src/components/Map/Map.svelte
+++
@@ -1,9 +0,0 @@
-<div>
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1wXqDeN9UWrStGuUWn6RsQlzRdan46Oc&ehbc=2E312F" 
-    class="m-3 mx-auto"
-    width="80%" height="480">
-    </iframe>
-</div> 
-
-
